754e54c8691272cc07032a2e94ff52e2cd497048
Mohammad Alzmzmi <91334712+lost-inthespace@users.noreply.github.com>
2025-01-18 01:55:23 +0300

    Update Program.cs

--- development/OpenAIWhisper/Program.cs
+++ development/OpenAIWhisper/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Azure;
 using Azure.AI.OpenAI;
-using Azure.Identity; // Required for Passwordless auth
+using Azure.Identity;
+from dotenv import load_dotenv
 
+load_dotenv()
+AzureKeyCrdntl = os.getenv("AzureKeyCrdntl")
 var endpoint = new Uri("https://qatarcentral.api.cognitive.microsoft.com/");
-var credentials = new AzureKeyCredential("AzPH519Y43GTcK8rxSvE3mk987omBRblsr9DKcqCr65IoBoiXeLWJQQJ99ALACYeBjFXJ3w3AAAYACOGSvz6");
+var credentials = new AzureKeyCredential("AzureKeyCrdntl");
 // var credentials = new DefaultAzureCredential(); // Use this line for Passwordless auth
 var deploymentName = "whisper"; // Default deployment name, update with your own if necessary
 var audioFilePath = "The Wind and the Sun - US English accent (TheFableCottage.com).mp3";
